e72fca1a23b20805dc1ecc60fde3ac0bbdaa3e77
JackPackham <60839411+JackPackham@users.noreply.github.com>
2020-03-04 18:59:02 +0000

    third pyqt5 version (clean up)
    It is as it says on the tin...

--- pyqt5/main.py
+++ pyqt5/main.py
@@ -6,6 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
+import source_rc
 
 from PyQt5 import QtCore, QtGui, QtWidgets, QtSql
 from secondWindow import SecondWindow
@@ -13,7 +14,7 @@ from secondWindow import SecondWindow
 class First(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(245, 373)
+        MainWindow.resize(245, 375)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
@@ -21,20 +22,31 @@ class First(object):
         font = QtGui.QFont()
         font.setFamily("Bebas Neue")
         font.setPointSize(12)
+
         self.label.setFont(font)
         self.label.setObjectName("label")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(60, 200, 121, 23))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(60, 230, 121, 23))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(60, 260, 121, 23))
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(90, 300, 61, 23))
-        self.pushButton_4.setObjectName("pushButton_4")
+
+        #create reminder button
+        self.createRem = QtWidgets.QPushButton(self.centralwidget)
+        self.createRem.setGeometry(QtCore.QRect(60, 200, 121, 23))
+        self.createRem.setObjectName("createRem")
+
+        #run reminder main app
+        self.runRem = QtWidgets.QPushButton(self.centralwidget)
+        self.runRem.setGeometry(QtCore.QRect(60, 230, 121, 23))
+        self.runRem.setObjectName("runRem")
+
+        #delete from json
+        self.deleteRem = QtWidgets.QPushButton(self.centralwidget)
+        self.deleteRem.setGeometry(QtCore.QRect(60, 260, 121, 23))
+        self.deleteRem.setObjectName("deleteRem")
+
+        #exit app
+        self.exitRem = QtWidgets.QPushButton(self.centralwidget)
+        self.exitRem.setGeometry(QtCore.QRect(90, 300, 61, 23))
+        self.exitRem.setObjectName("exitRem")
+
+        #labels
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(60, 60, 131, 131))
         self.label_2.setStyleSheet("image: url(:/newPrefix/titleScreen.jpg);")
@@ -42,6 +54,7 @@ class First(object):
         self.label_2.setObjectName("label_2")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setGeometry(QtCore.QRect(80, 10, 101, 31))
+
         font = QtGui.QFont()
         font.setFamily("Bebas Neue")
         font.setPointSize(24)
@@ -56,8 +69,8 @@ class First(object):
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
-        self.pushButton.clicked.connect(self.openWindow)
-        self.pushButton_4.clicked.connect(self.windowExit)
+        self.createRem.clicked.connect(self.openWindow)
+        self.exitRem.clicked.connect(self.windowExit)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -66,10 +79,10 @@ class First(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Task 366"))
         self.label.setText(_translate("MainWindow", "What would you like to do?"))
-        self.pushButton.setText(_translate("MainWindow", "create reminder"))
-        self.pushButton_2.setText(_translate("MainWindow", "run reminders"))
-        self.pushButton_3.setText(_translate("MainWindow", "delete reminders"))
-        self.pushButton_4.setText(_translate("MainWindow", "exit"))
+        self.createRem.setText(_translate("MainWindow", "create reminder"))
+        self.runRem.setText(_translate("MainWindow", "run reminders"))
+        self.deleteRem.setText(_translate("MainWindow", "delete reminders"))
+        self.exitRem.setText(_translate("MainWindow", "exit"))
         self.label_3.setText(_translate("MainWindow", "Task 366"))
 
     def openWindow(self):
@@ -77,12 +90,10 @@ class First(object):
         self.ui = SecondWindow()
         self.ui.setupUi(self.window)
         self.window.show()
+        MainWindow.hide()
 
     def windowExit(self):
         sys.exit()
-
-import source_rc
-
 
 if __name__ == "__main__":
     import sys
--- pyqt5/secondWindow.py
+++ pyqt5/secondWindow.py
@@ -11,54 +11,89 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class SecondWindow(object):
+
+    def collectInput(self):
+        print("incomplete")
+
+    def goBack(self):
+        print("incomplete")
+        #MainWindow.show()
+        #SecondWindow.hide()
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(534, 281)
         MainWindow.setTabShape(QtWidgets.QTabWidget.Rounded)
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
+        #Name entry
         self.TaskNameEntry = QtWidgets.QLineEdit(self.centralwidget)
         self.TaskNameEntry.setGeometry(QtCore.QRect(100, 40, 113, 20))
         self.TaskNameEntry.setObjectName("TaskNameEntry")
+
+        #date entry
         self.TaskDateEntry = QtWidgets.QLineEdit(self.centralwidget)
         self.TaskDateEntry.setGeometry(QtCore.QRect(100, 90, 113, 20))
         self.TaskDateEntry.setObjectName("TaskDateEntry")
+
+        #between
         self.betweenTime = QtWidgets.QLineEdit(self.centralwidget)
         self.betweenTime.setEnabled(False)
         self.betweenTime.setGeometry(QtCore.QRect(10, 150, 51, 20))
         self.betweenTime.setObjectName("betweenTime")
+
+        #toTime
+        self.toTime = QtWidgets.QLineEdit(self.centralwidget)
+        self.toTime.setEnabled(False)
+        self.toTime.setGeometry(QtCore.QRect(110, 150, 51, 20))
+        self.toTime.setObjectName("toTime")
+
+        #is timed checkbox
         self.Timed = QtWidgets.QCheckBox(self.centralwidget)
         self.Timed.setEnabled(True)
         self.Timed.setGeometry(QtCore.QRect(40, 120, 91, 17))
         self.Timed.setLayoutDirection(QtCore.Qt.RightToLeft)
         self.Timed.setObjectName("Timed")
+
+        #labels
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(10, 40, 81, 20))
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(10, 90, 81, 20))
         self.label_2.setObjectName("label_2")
+
+        #to menu
         self.back = QtWidgets.QPushButton(self.centralwidget)
         self.back.setGeometry(QtCore.QRect(10, 210, 75, 23))
         self.back.setObjectName("back")
+        
+        #self.back.clicked.connect(self.goBack)
+
+        #finish buttom
         self.finishEntry = QtWidgets.QPushButton(self.centralwidget)
         self.finishEntry.setGeometry(QtCore.QRect(10, 180, 75, 23))
         self.finishEntry.setObjectName("finishEntry")
-        self.toTime = QtWidgets.QLineEdit(self.centralwidget)
-        self.toTime.setEnabled(False)
-        self.toTime.setGeometry(QtCore.QRect(110, 150, 51, 20))
-        self.toTime.setObjectName("toTime")
+        self.finishEntry.clicked.connect(self.collectInput)
+
+        #task names box
         self.TaskNameView = QtWidgets.QListView(self.centralwidget)
         self.TaskNameView.setGeometry(QtCore.QRect(230, 30, 81, 192))
         self.TaskNameView.setObjectName("TaskNameView")
+
+        #task name dates box
         self.TaskDateView = QtWidgets.QListView(self.centralwidget)
         self.TaskDateView.setGeometry(QtCore.QRect(330, 30, 191, 192))
         self.TaskDateView.setObjectName("TaskDateView")
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 534, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
+
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
@@ -77,11 +112,3 @@ class SecondWindow(object):
         self.finishEntry.setText(_translate("MainWindow", "Finish entry"))
 
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
